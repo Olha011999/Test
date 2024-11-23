1cefa5ab47973d58ae25b3c24b129434ee1e8678
Alla-Nazarenko <umnyj.start@gmail.com>
2024-11-23 17:34:56 +0100

    changed variables

--- src/TestOlha.java
+++ src/TestOlha.java
@@ -6,14 +6,17 @@ public class TestOlha {
         a=10;
         b=20;
 
+        //-----Alla changed
+        a=50;
+        b=100;
+
         System.out.println(calculateSum(a, b));
-
-
     }
 
     private static int calculateSum(int a, int b) {
         int res = a + b;
-
         return res;
     }
+
+
 }
