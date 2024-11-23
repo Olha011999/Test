50121fd17d1e9e40a257e04da9d181a98dedc3ce
Olha Fatina <fowell@urk.net>
2024-11-23 16:12:29 +0100

    result

---
+++ src/TestOlha.java
@@ -0,0 +1,18 @@
+public class TestOlha {
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        a=10;
+        b=20;
+
+        calculateSum(a, b);
+
+
+    }
+
+    private static void calculateSum(int a, int b) {
+        int res = a + b;
+        System.out.println(res);
+    }
+}
